010dd2d48b1132e9b1d1e3d28add7e864c247682
Toifyx <fu_yx@hisuntech.com>
2019-12-11 18:11:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+# test
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+1,安装node.js:
+
+官网下下载，直接“下一步”完成安装后，在终端输入node -v ，如果出现对应的版本号则表示已经安装过了。
+
+2,安装cnpm:
+
+npm: Nodejs下的包管理器。由于国内使用npm会很慢,这里推荐使用淘宝NPM镜像（http://npm.taobao.org/） 
+
+终端中输入： $ npm install -g cnpm –registry=https://registry.npm.taobao.org
+
+3,安装vue-cli脚手架构建工具
+
+npm install -g vue-cli
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="hello">
+  <h1>{{ msg }}</h1>
+  <h2>Essential Links</h2>
+  <ul>
+    <li>
+      <a href="https://vuejs.org" target="_blank">Core Docs</a>
+    </li>
+  </ul>
+  <body>
+    <li id="sayhello">{{message}}</li>
+  </body>
+  <body>
+    <span id="app-2" v-bind:title="message_app2">鼠标悬停几秒钟查看此处动态绑定的提示信息！</span>
+  </body>
+  <body id = "seeBody">
+    <p id="textSee">{{seeText}}</p>
+  </body>
+  <button v-on:click="button_1()">点我</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  seeFlg: true,
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      message_app2: new Date().toLocaleString(),
+      message: "Hello Vue!",
+      seeText: "现在你看到我了",
+      seeFlg: true
+    };
+  },
+  methods: {
+    button_1: function() {
+      console.log("1:", this.seeFlg);
+      if (this.seeFlg) {
+        (this.seeFlg = false), (this.seeText = " ");
+      } else {
+        (this.seeFlg = true), (this.seeText = "现在你看到我了");
+      }
+      console.log("1:", this.seeFlg);
+      console.log("");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #184ddf;
+}
+#sayhello {
+  color: #e79110;
+}
+#seeBody {
+  height: 22px;
+}
+</style>
